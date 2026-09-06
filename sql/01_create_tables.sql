4a6d609ad29535cc3b749ba5d2fdf288a026100b
hamilton-carlos <hamilton.carlos.gomes@gmail.com>
2026-09-06 17:53:37 -0300

    feat: adiciona tabela de produtos

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -11,3 +11,14 @@ CREATE TABLE categorias (
     nome VARCHAR(100) NOT NULL UNIQUE,
     descricao VARCHAR(255)
 );
+
+CREATE TABLE produtos (
+    id_produto SERIAL PRIMARY KEY,
+    nome VARCHAR(120) NOT NULL,
+    descricao VARCHAR(255),
+    preco NUMERIC(10,2) NOT NULL,
+    estoque INTEGER NOT NULL DEFAULT 0,
+    id_categoria INTEGER NOT NULL,
+    FOREIGN KEY (id_categoria)
+        REFERENCES categorias(id_categoria)
+);
