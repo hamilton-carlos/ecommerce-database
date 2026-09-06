9272fba90faf26927d7b223d2ad81bd2342b1344
hamilton-carlos <hamilton.carlos.gomes@gmail.com>
2026-09-06 17:54:27 -0300

    feat: adiciona tabela de pedidos

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -22,3 +22,13 @@ CREATE TABLE produtos (
     FOREIGN KEY (id_categoria)
         REFERENCES categorias(id_categoria)
 );
+
+CREATE TABLE pedidos (
+    id_pedido SERIAL PRIMARY KEY,
+    id_cliente INTEGER NOT NULL,
+    data_pedido TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(30) NOT NULL DEFAULT 'Pendente',
+    valor_total NUMERIC(10,2) NOT NULL DEFAULT 0,
+    FOREIGN KEY (id_cliente)
+        REFERENCES clientes(id_cliente)
+);
